db0c928ec20c5d32f8855f1567cfb6bdba4f918c
Xin Wu <xinwu@us.ibm.com>
2016-05-24 00:23:04 -0700

    SPARK-15431: handle remote case

--- sql/core/src/main/scala/org/apache/spark/sql/execution/command/resources.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/command/resources.scala
@@ -69,7 +69,14 @@ case class ListFilesCommand(files: Seq[String] = Seq.empty[String]) extends Runn
           case null | "local" => new File(f).getCanonicalFile.toURI.toString
           case _ => f
         }
-        new Path(schemeCorrectedPath).toUri.toString
+
+        // In a non-local mode, ADD FILE command will generate the path via rpcEnv fileserver
+        val realPath = if (!sparkSession.sparkContext.isLocal && uri.getScheme == "file") {
+          sparkSession.sparkContext.env.rpcEnv.fileServer.addFile(new File(uri.getPath))
+        } else {
+          schemeCorrectedPath
+        }
+        new Path(realPath).toUri.toString
       }.collect {
         case f if fileList.contains(f) => f
       }.map(Row(_))
